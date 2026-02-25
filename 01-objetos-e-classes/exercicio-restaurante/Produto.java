f14854c50867c9fa533ee1b849d946835719a091
Emerson Jesus <emersomjesus999@gmail.com>
2026-02-25 18:35:19 -0300

    Create Produto.java

---
+++ 01-objetos-e-classes/exercicio-restaurante/Produto.java
@@ -0,0 +1,35 @@
+package pratica2;
+/*- A lanchonete Delicias está precisando de um programa para controlar os pedidos dos clientes. Todos os
+ *  pedidos são feitos manualmente pela funcionária Gertrudes e ocorrem vários problemas no momento do 
+ *  pagamento.  A proprietária da lanchonete solicitou um programa que permita:
+⦁	Cadastrar os produtos vendidos na lanchonete
+⦁	Registrar cada pedido: produtos consumidos e quantidades 
+⦁	Calcular o total de cada pedido
+Pense o que é necessário saber sobre os produtos de uma lanchonete, qual será o comportamento dos produtos. 
+Defina os atributos e métodos da classe e depois implemente-a em Java. Crie também um programa principal para 
+testar sua classe Produto. 
+*/
+
+public class Produto {
+	String nome;
+	double preco;
+	
+}
+
+
+
+	/*
+	void RegistrarPedido(){
+		System.out.println("os produtos consumidos sao: "+nome);
+		System.out.println("e a quantidade e "+quantidade);
+		
+		
+	}
+	
+	void CalcularTotal(){
+		double total=quantidade*preco;
+		System.out.println("o total foi :"+total);
+	}*/
+	
+
+
