4de36ce073d39ea58f0648e2ddb8f4bb8565cca7
Emerson Jesus <emersomjesus999@gmail.com>
2026-02-26 18:34:02 -0300

    Update Continente.java

--- 03-pacote-e-arrayList/exercicio-pais-continente/Continente.java
+++ 03-pacote-e-arrayList/exercicio-pais-continente/Continente.java
@@ -1,3 +1,14 @@
+/*Escreva em Java uma classe Continente. Um continente possui um nome e é
+composto por um conjunto de países. Forneça os membros de classe a seguir:
+ Construtor que inicialize o nome do continente;
+ Um método que permita adicionar países aos continentes;
+ Um método que retorne a dimensão total do continente;
+ Um método que retorne a população total do continente;
+ Um método que retorne a densidade populacional do continente;
+ Um método que retorne o país com maior população no continente;
+ Um método que retorne o país com menor população no continente;
+ Um método que retorne o país de maior dimensão territorial no continente;
+ Um método que retorne o país de menor dimensão territorial no continente.*/
 package excerciciosRevisao;
 import java.util.ArrayList;
 //Escreva em Java uma classe Continente. Um continente possui um nome e é composto por um conjunto de países. Forneça os membros de classe a seguir: 
