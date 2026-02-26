b49b8b57c856d4746af3686ec9861d16d7a10085
Emerson Jesus <emersomjesus999@gmail.com>
2026-02-26 20:11:02 -0300

    Create LivroFisico.java

---
+++ 06-interface/projeto-livraria/LivroFisico.java
@@ -0,0 +1,24 @@
+package pratica9;
+
+//classe LivroFisico
+public class LivroFisico extends Livro implements IPromocional{
+  
+  public LivroFisico(){
+      super();
+  }
+  
+  public double getTaxaImpressao(){
+      return (super.valor *0.05);
+  }
+  
+  @Override
+  public boolean aplicaDesconto(double porcentagem){
+       if (porcentagem >0.15){
+          return false;
+      }
+      else {
+          super.valor -= (super.valor * porcentagem);
+          return true;
+      }
+  }
+}
