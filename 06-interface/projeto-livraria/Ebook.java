7a50917a61a834b86db13edc6744fd1908c5255b
Emerson Jesus <emersomjesus999@gmail.com>
2026-02-26 20:09:17 -0300

    Create Ebook.java

---
+++ 06-interface/projeto-livraria/Ebook.java
@@ -0,0 +1,28 @@
+package pratica9;
+
+//Classe Ebook
+public class Ebook extends Livro implements IPromocional{
+  private String marcaDagua;
+
+  public Ebook(){
+      
+  }
+  public Ebook(String marcaDagua){
+      super();
+      this.marcaDagua=marcaDagua;
+  }
+      
+  @Override
+  public boolean aplicaDesconto(double porcentagem){
+      if (porcentagem >0.30){
+          return false;
+      }
+      else {
+          super.valor -= (super.valor * porcentagem);
+          return true;
+      }
+  }
+  public String getMarcaDagua(){
+          return marcaDagua;
+  }
+}
