4fa5f3a98e021b4da98b633b583ff6b560266855
Emerson Jesus <emersomjesus999@gmail.com>
2026-02-26 20:11:35 -0300

    Create MiniLivro.java

---
+++ 06-interface/projeto-livraria/MiniLivro.java
@@ -0,0 +1,15 @@
+package pratica9;
+
+//Classe MiniLivro
+public class MiniLivro extends Livro{
+  
+  public MiniLivro(){
+      super();
+  }
+
+  //@Override
+  public boolean aplicaDesconto(double porcentagem){
+      return false;
+  }
+  
+}
